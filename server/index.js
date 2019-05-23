edb7c6733d97d9d9e41431e649b496c68ab46ca2
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-23 14:07:13 -0600

    update v1

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+const session = require('express-session')
+const massive = require('massive')
+const app = express()
+
+app.use(express.json())
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 600
+        }
+    }
+)
+
+massive(CONNECTION_STRING).then(database =>{
+    app.set('db', database)
+    console.log('Database moving along')
+})
+
+app.listen(SERVER_PORT, () => { console.log(`Cruising on port ${SERVER_PORT}`)
+})
